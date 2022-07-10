5b42ae19674ba01529c14836f8a394beb27a7c14
diverseman <1365582768@qq.com>
2022-07-10 15:01:04 +0800

    ArrayDeque框架已经搭好，同样名字的方法会影响LinkedListDequeTest的测试，下次修

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,6 +1,29 @@
 public class ArrayDeque <Good>{
-
-
     int size=8;
+    public void addFirst(T item){
+        
+    }
+    public void addLast(T item){
+        
+    }
+    public boolean isEmpty(){
+        
+    }
+    public int size(){
+        
+    }
+    public void printDeque(){
+        
+    }
+    public T removeFirst(){
+        
+    }
+    public T removeLast(){
+        
+    }
+    public T get(int index){
+        
+    }
+
 
 }
